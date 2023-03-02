998bab82b384f6e7e7210a2b424b027d6d7b60ad
jungleboy36 <93709766+jungleboy36@users.noreply.github.com>
2023-03-02 11:37:34 +0100

    first commit

---
+++ src/Controller/IndexController.php
@@ -0,0 +1,13 @@
+<?php
+namespace App\Controller;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+Use Symfony\Component\Routing\Annotation\Route;
+class IndexController extends AbstractController
+{
+ 
+ public function home()
+ {
+    $articles = ['Artcile1', 'Article 2','Article 3'];
+    return $this->render('articles/index.html.twig',['articles' => $articles]);  }
+}
